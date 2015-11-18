b0d52e56f0631230e4ea74056a200dcab90362fc
everett980 <everett980@gmail.com>
2015-11-18 11:25:32 -0500

    Complete Model Testing

--- test/itsalive.js
+++ test/itsalive.js
@@ -1,38 +1,38 @@
-var chai = require('chai');
-var expect = chai.expect;
-var spies = require('chai-spies');
-chai.use(spies);
+// var chai = require('chai');
+// var expect = chai.expect;
+// var spies = require('chai-spies');
+// chai.use(spies);
 
 
-describe("Testing suite capabilities", function() {
-	it('confirms basic arithmetic', function() {
-		expect(2+2).to.be.equal(4);
-	});
-});
+// describe("Testing suite capabilities", function() {
+// 	it('confirms basic arithmetic', function() {
+// 		expect(2+2).to.be.equal(4);
+// 	});
+// });
 
-describe("Testing time", function() {
-	xit("confirms expectations of time", function(done) {
-		var startTime = new Date();
-		setTimeout(function() {
-			var duration = new Date() - startTime;
-			expect(duration).to.be.closeTo(1000, 50);
-			done();
-		}, 1000);
-	});
-});
+// describe("Testing time", function() {
+// 	xit("confirms expectations of time", function(done) {
+// 		var startTime = new Date();
+// 		setTimeout(function() {
+// 			var duration = new Date() - startTime;
+// 			expect(duration).to.be.closeTo(1000, 50);
+// 			done();
+// 		}, 1000);
+// 	});
+// });
 
-var obj = {
-	foobar : function() {
-		console.log('foo');
-		return 'bar';
-	}
-}
-chai.spy.on(obj, 'foobar');
+// var obj = {
+// 	foobar : function() {
+// 		console.log('foo');
+// 		return 'bar';
+// 	}
+// }
+// chai.spy.on(obj, 'foobar');
 
-describe("spying", function() {
-	it("counts the times foobar is run", function () {
-		obj.foobar();
-		expect(obj.foobar).to.have.been.called.exactly(1);
-	})
-})
+// describe("spying", function() {
+// 	it("counts the times foobar is run", function () {
+// 		obj.foobar();
+// 		expect(obj.foobar).to.have.been.called.exactly(1);
+// 	})
+// })
 
--- test/wikiTest.js
+++ test/wikiTest.js
@@ -2,41 +2,151 @@ var models = require('../models');
 var Page = models.Page;
 var User = models.User;
 
+var chai = require('chai');
+var expect = chai.expect;
+var spies = require('chai-spies');
+chai.use(spies);
+
+
 
 describe('Page model', function() {
 
     describe('Validations', function() {
         var p;
         beforeEach(function() {
-            p = new Page();
+            p = new Page( {
+
+            });
         })
-        xit('errors without title', function() {});
-        xit('errors without content', function() {});
+        it('errors without title', function(done) {
+            p.validate(function(err) {
+                expect(err.errors).to.have.property('title')
+                done()
+            })
+        });
+        it('errors without content', function(done) {
+            p.validate(function(err) {
+                expect(err.errors).to.have.property('title')
+                done()
+            })
+        });
     });
 
     describe('Statics', function() {
         describe('findByTag', function() {
-            xit('gets pages with the search tag', function() {});
-            xit('does not get pages without the search tag', function() {});
+            var p1 = new Page({
+                title: 'test',
+                content: 'some content',
+                tags: ['tag1', 'tag2']
+            })
+            var p2 = new Page({
+                title: 'test2',
+                content: 'more content',
+                tags: ['tag3']
+            })
+            p1.save();
+            p2.save();
+            it('gets pages with the search tag', function(done) {
+                Page.findByTag('tag1').then(function(matches) {
+                    expect(matches).to.have.length.above(0);
+                    done();
+                })
+            });
+            it('does not get pages without the search tag', function() {
+                Page.findByTag('noPagesHaveThisTag').then(function(matches) {
+                    expect(matches).to.have.length.equal(0);
+                    done();
+                })
+            });
         });
     });
 
     describe('Methods', function() {
         describe('findSimilar', function() {
-            xit('never gets itself', function() {});
-            xit('gets other pages with any common tags', function() {});
-            xit('does not get other pages without any common tags', function() {});
+            var p1, p2, p3;
+            beforeEach(function() {
+                p1 = new Page({
+                    title: 'test',
+                    content: 'some content',
+                    tags: ['tag1', 'tag2']
+                })
+                p2 = new Page({
+                    title: 'test2',
+                    content: 'more content',
+                    tags: ['tag2']
+                })
+                p3 = new Page({
+                    title: 'test3',
+                    content: 'more content',
+                    tags: ['tag3']
+                })
+                p1.save();
+                p2.save();
+                p3.save();
+            });
+            afterEach(function () {
+                Page.remove({title: 'test'});
+                Page.remove({title: 'test2'});
+                Page.remove({title: 'test3'});
+            })
+            it('never gets itself', function() {
+                p1.findSimilar().then(function (matches) {
+                    expect(matches).to.have.length.equal(1);
+                    expect(matches[0].title).to.not.equal('test');
+                })
+            });
+            it('gets other pages with any common tags', function() {
+                p1.findSimilar().then(function (matches) {
+                    expect(matches).to.have.length.equal(1);
+                    expect(matches[0].title).to.equal('test2');
+                })
+            });
+            it('does not get other pages without any common tags', function() {
+                p3.findSimilar().then(function (matches) {
+                    expect(matches).to.have.length.equal(0);
+                });
+            });
         });
     });
 
     describe('Virtuals', function() {
+        var p1;
+        beforeEach(function() {
+            p1 = new Page({
+                title: 'test',
+                urlTitle: 'helloWorld',
+                content: 'some content',
+                tags: ['tag1', 'tag2']
+            });
+        });
         describe('route', function() {
-            xit('returns the url_name prepended by "/wiki/"', function() {});
+            it('returns the url_name prepended by "/wiki/"', function() {
+                expect(p1.route).to.equal('/wiki/helloWorld');
+            });
         });
     });
 
     describe('Hooks', function() {
-        xit('it sets urlTitle based on title before validating', function() {});
+        var p1;
+        beforeEach(function() {
+            p1 = new Page({
+                title: 'test',
+                content: 'some content',
+                tags: ['tag1', 'tag2']
+            });
+        });    
+        afterEach(function () {
+                Page.remove({title: 'test'});
+            })
+        it('it sets urlTitle based on title before validating', function() {
+            var spy = chai.spy(p1.pre);
+            p1.save().then(function() {
+                expect(spy).to.have.been.called.exactly(1);
+            });
+        });
     });
 
 });
+
+
+// chai.spy.on(mongoose, 'method')
